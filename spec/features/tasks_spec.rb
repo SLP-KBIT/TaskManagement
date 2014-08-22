fa0391eff967689d02af62c3ea2828fdd8a11cea
Club09 s09t214 OOKAWA Masahiro <clc.iguto@gmail.com>
2014-08-23 00:16:07 +0900

    feature spec のexampleを追加

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -1,14 +1,38 @@
 require 'rails_helper'
+require 'faker'
 
 RSpec.describe 'Task', type: :feature do
   before do
     FactoryGirl.create(:feature_task)
     FactoryGirl.create(:finished_task)
   end
+
   scenario 'view task list' do
     visit root_path
 
     expect(page).to have_content '新しいタスク'
     expect(page).to have_content 'タスク名'
+  end
+
+  scenario 'view finished task list' do
+    visit root_path
+
+    click_on '完了済み'
+
+    expect(page).to have_content '新しいタスク'
+    expect(page).to have_content 'タスク名'
+  end
+
+  scenario 'add new task' do
+    visit root_path
+    click_on '新しいタスク'
+
+    expect(page).to have_content 'タスクの新規作成'
+
+    expect {
+      fill_in 'タイトル', with: Faker::Lorem.sentence
+      fill_in '詳細', with: Faker::Lorem.paragraph
+      click_on '作成'
+    }.to change(Task, :count).by(1)
   end
 end
